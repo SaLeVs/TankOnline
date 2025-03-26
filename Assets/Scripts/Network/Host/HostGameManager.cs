8a9faa05c76ff635a56c1643512cef7d3fe03271
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-03-26 07:12:59 -0300

    Lobbies creating
    v35

--- Assets/Scripts/Network/Host/HostGameManager.cs
+++ Assets/Scripts/Network/Host/HostGameManager.cs
@@ -2,10 +2,15 @@ using System.Threading.Tasks;
 using Unity.Netcode;
 using Unity.Netcode.Transports.UTP;
 using Unity.Networking.Transport.Relay;
+using Unity.Services.Lobbies;
+using Unity.Services.Lobbies.Models;
 using Unity.Services.Relay;
 using Unity.Services.Relay.Models;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using System.Collections;
+
 
 public class HostGameManager
 {
@@ -15,6 +20,7 @@ public class HostGameManager
 
     private Allocation allocation;
     private string joinCode;
+    private string lobbyId;
 
     public async Task StartHostAsysnc()
     {
@@ -44,7 +50,42 @@ public class HostGameManager
         UnityTransport unityTransport = NetworkManager.Singleton.GetComponent<UnityTransport>();
         unityTransport.SetRelayServerData(relayServerData);
 
+        try
+        {
+            CreateLobbyOptions lobbyOptions = new CreateLobbyOptions();
+            lobbyOptions.IsPrivate = false; // we can add something here for create private lobbies
+            lobbyOptions.Data = new Dictionary<string, DataObject>()
+            {
+                { "JoinCode", new DataObject(
+                visibility: DataObject.VisibilityOptions.Member,
+                value: joinCode)
+                }
+            };
+
+           Lobby lobby = await Lobbies.Instance.CreateLobbyAsync("My Lobby", MAX_CONNECTIONS, lobbyOptions);
+           lobbyId = lobby.Id;
+
+            HostSingleton.Instance.StartCoroutine(HeartBeatLobby(15f));
+        }
+        catch (LobbyServiceException e)
+        {
+            Debug.Log(e);
+            return;
+        }
         NetworkManager.Singleton.StartHost();
         NetworkManager.Singleton.SceneManager.LoadScene(GameSceneName, LoadSceneMode.Single);
     }
+
+
+    private IEnumerator HeartBeatLobby(float waitTimeSeconds) // we use this for say to ugs, that the lobby is still active
+    {
+        WaitForSecondsRealtime delay = new WaitForSecondsRealtime(waitTimeSeconds); // we use this for not create a new object every time
+
+        while (true)
+        {
+            Lobbies.Instance.SendHeartbeatPingAsync(lobbyId);
+            yield return delay;
+        }
+    }
+
 }
